06bdead69f4da8fbe98b64d20920f85c5d2ed26f
hujingang <hujingang@guazi.com>
2016-11-09 10:44:54 +0800

    fix next page error

--- src/internal.js
+++ src/internal.js
@@ -503,8 +503,8 @@ function renderPaginationItem(list, page, text, markerCss)
                 {
                     var commandList = {
                         first: 1,
-                        prev: that.current - 1,
-                        next: that.current + 1,
+                        prev: Number(that.current) - 1,
+                        next: Number(that.current) + 1,
                         last: that.totalPages
                     };
                     var command = $this.data("page");
